6ae7bf6ff17187d6edea3bf24a44d103be0ff1c4
thefirstjedi42 <thefirstjedi41@blueprint41.com>
2024-04-02 08:47:15 +0800

    test code

--- lib/instagram/oauth.rb
+++ lib/instagram/oauth.rb
@@ -7,7 +7,7 @@ module Instagram
       options[:scope] ||= scope if !scope.nil? && !scope.empty?
       options[:redirect_uri] ||= self.redirect_uri
       params = authorization_params.merge(options)
-      connection.build_url("/oauth/authorize/", params).to_s
+      connection.build_url("/oauth/authorize", params).to_s
     end
 
     # Return an access token from authorization
@@ -15,7 +15,7 @@ module Instagram
       options[:grant_type] ||= "authorization_code"
       options[:redirect_uri] ||= self.redirect_uri
       params = access_token_params.merge(options)
-      post("/oauth/access_token/", params.merge(:code => code), signature=false, raw=false, unformatted=true, no_response_wrapper=true)
+      post("/oauth/access_token", params.merge(:code => code), signature=false, raw=false, unformatted=true, no_response_wrapper=true)
     end
 
     private
